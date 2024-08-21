b8b37f93a44afcd180d9eeac4dabb2a093fae7ad
DRPS PHCA <drpsbn.ph@gmail.com>
2024-08-21 13:41:50 +0800

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PBB_Gen_11_Housemates_Info
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.MainLBL = new System.Windows.Forms.Label();
+            this.HMCB = new System.Windows.Forms.ComboBox();
+            this.HMSelLBL = new System.Windows.Forms.Label();
+            this.HMPB = new System.Windows.Forms.PictureBox();
+            this.HMNameLBL = new System.Windows.Forms.Label();
+            this.HMMonikerLBL = new System.Windows.Forms.Label();
+            this.DRPSPHCALogo = new System.Windows.Forms.PictureBox();
+            this.HMStatusLBL = new System.Windows.Forms.Label();
+            this.AppVerLBL = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.HMPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DRPSPHCALogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainLBL
+            // 
+            this.MainLBL.AutoSize = true;
+            this.MainLBL.BackColor = System.Drawing.Color.Transparent;
+            this.MainLBL.Font = new System.Drawing.Font("Pixellari", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainLBL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MainLBL.Location = new System.Drawing.Point(24, 18);
+            this.MainLBL.Name = "MainLBL";
+            this.MainLBL.Size = new System.Drawing.Size(494, 35);
+            this.MainLBL.TabIndex = 0;
+            this.MainLBL.Text = "PBB Gen 11 Housemates Info";
+            // 
+            // HMCB
+            // 
+            this.HMCB.BackColor = System.Drawing.Color.Indigo;
+            this.HMCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.HMCB.Font = new System.Drawing.Font("Gilroy ExtraBold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HMCB.ForeColor = System.Drawing.SystemColors.Window;
+            this.HMCB.FormattingEnabled = true;
+            this.HMCB.Items.AddRange(new object[] {
+            "Binsoy Namoca",
+            "Brx Ruiz",
+            "Dingdong and Patrick",
+            "Dylan Yturralde",
+            "Fyang Smith",
+            "Jan Silva",
+            "Jarren Garcia",
+            "Jas Dudley-Scales",
+            "JM Ibarra",
+            "Joli Alferez",
+            "JP Cabrera",
+            "Kai Montinola",
+            "Kanata Tapia",
+            "Kolette Madelo",
+            "Marc Nanninga",
+            "Noimie Steikunas",
+            "Rain Celmar",
+            "Therese Villamor"});
+            this.HMCB.Location = new System.Drawing.Point(238, 59);
+            this.HMCB.Name = "HMCB";
+            this.HMCB.Size = new System.Drawing.Size(434, 32);
+            this.HMCB.TabIndex = 1;
+            this.HMCB.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // HMSelLBL
+            // 
+            this.HMSelLBL.AutoSize = true;
+            this.HMSelLBL.BackColor = System.Drawing.Color.Transparent;
+            this.HMSelLBL.Font = new System.Drawing.Font("Gilroy ExtraBold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HMSelLBL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HMSelLBL.Location = new System.Drawing.Point(30, 63);
+            this.HMSelLBL.Name = "HMSelLBL";
+            this.HMSelLBL.Size = new System.Drawing.Size(204, 26);
+            this.HMSelLBL.TabIndex = 2;
+            this.HMSelLBL.Text = "Select a housemate";
+            // 
+            // HMPB
+            // 
+            this.HMPB.BackColor = System.Drawing.Color.Transparent;
+            this.HMPB.Image = global::PBB_Gen_11_Housemates_Info.Properties.Resources.PHCAPBBGen11HM_Unselected;
+            this.HMPB.Location = new System.Drawing.Point(35, 102);
+            this.HMPB.Name = "HMPB";
+            this.HMPB.Size = new System.Drawing.Size(120, 120);
+            this.HMPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.HMPB.TabIndex = 3;
+            this.HMPB.TabStop = false;
+            // 
+            // HMNameLBL
+            // 
+            this.HMNameLBL.AutoSize = true;
+            this.HMNameLBL.BackColor = System.Drawing.Color.Transparent;
+            this.HMNameLBL.Font = new System.Drawing.Font("Gilroy ExtraBold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HMNameLBL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HMNameLBL.Location = new System.Drawing.Point(161, 102);
+            this.HMNameLBL.Name = "HMNameLBL";
+            this.HMNameLBL.Size = new System.Drawing.Size(271, 26);
+            this.HMNameLBL.TabIndex = 4;
+            this.HMNameLBL.Text = "Please select a housemate";
+            // 
+            // HMMonikerLBL
+            // 
+            this.HMMonikerLBL.AutoSize = true;
+            this.HMMonikerLBL.BackColor = System.Drawing.Color.Transparent;
+            this.HMMonikerLBL.Font = new System.Drawing.Font("Gilroy Light", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HMMonikerLBL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HMMonikerLBL.Location = new System.Drawing.Point(161, 128);
+            this.HMMonikerLBL.Name = "HMMonikerLBL";
+            this.HMMonikerLBL.Size = new System.Drawing.Size(0, 20);
+            this.HMMonikerLBL.TabIndex = 5;
+            // 
+            // DRPSPHCALogo
+            // 
+            this.DRPSPHCALogo.BackColor = System.Drawing.Color.Transparent;
+            this.DRPSPHCALogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.DRPSPHCALogo.Image = global::PBB_Gen_11_Housemates_Info.Properties.Resources.DRPSPHCABranding2023;
+            this.DRPSPHCALogo.Location = new System.Drawing.Point(552, 192);
+            this.DRPSPHCALogo.Name = "DRPSPHCALogo";
+            this.DRPSPHCALogo.Size = new System.Drawing.Size(120, 30);
+            this.DRPSPHCALogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DRPSPHCALogo.TabIndex = 6;
+            this.DRPSPHCALogo.TabStop = false;
+            // 
+            // HMStatusLBL
+            // 
+            this.HMStatusLBL.AutoSize = true;
+            this.HMStatusLBL.BackColor = System.Drawing.Color.Transparent;
+            this.HMStatusLBL.Font = new System.Drawing.Font("Gilroy Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HMStatusLBL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HMStatusLBL.Location = new System.Drawing.Point(162, 195);
+            this.HMStatusLBL.Name = "HMStatusLBL";
+            this.HMStatusLBL.Size = new System.Drawing.Size(0, 24);
+            this.HMStatusLBL.TabIndex = 7;
+            // 
+            // AppVerLBL
+            // 
+            this.AppVerLBL.AutoSize = true;
+            this.AppVerLBL.BackColor = System.Drawing.Color.Transparent;
+            this.AppVerLBL.Font = new System.Drawing.Font("Gilroy Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppVerLBL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.AppVerLBL.Location = new System.Drawing.Point(527, 176);
+            this.AppVerLBL.Name = "AppVerLBL";
+            this.AppVerLBL.Size = new System.Drawing.Size(147, 13);
+            this.AppVerLBL.TabIndex = 8;
+            this.AppVerLBL.Text = "Ver. PHCAPBB11-20240821-1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PBB_Gen_11_Housemates_Info.Properties.Resources.DRPSPHCAPBBGen11HMGraphic_2024BKG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(697, 239);
+            this.Controls.Add(this.AppVerLBL);
+            this.Controls.Add(this.HMStatusLBL);
+            this.Controls.Add(this.DRPSPHCALogo);
+            this.Controls.Add(this.HMMonikerLBL);
+            this.Controls.Add(this.HMNameLBL);
+            this.Controls.Add(this.HMPB);
+            this.Controls.Add(this.HMSelLBL);
+            this.Controls.Add(this.HMCB);
+            this.Controls.Add(this.MainLBL);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PBB Gen 11 Housemates Info";
+            ((System.ComponentModel.ISupportInitialize)(this.HMPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DRPSPHCALogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainLBL;
+        private System.Windows.Forms.ComboBox HMCB;
+        private System.Windows.Forms.Label HMSelLBL;
+        private System.Windows.Forms.PictureBox HMPB;
+        private System.Windows.Forms.Label HMNameLBL;
+        private System.Windows.Forms.Label HMMonikerLBL;
+        private System.Windows.Forms.PictureBox DRPSPHCALogo;
+        private System.Windows.Forms.Label HMStatusLBL;
+        private System.Windows.Forms.Label AppVerLBL;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBB_Gen_11_Housemates_Info
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (HMCB.Text == "Binsoy Namoca")
+            {
+                HMNameLBL.Text = "Binsoy Namoca";
+                HMMonikerLBL.Text = "The Maabilidancing Dong of South Cotabato";
+                HMStatusLBL.Text = "Safe";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_BinsoyNamoca.png");
+            }
+            else if (HMCB.Text == "Brx Ruiz")
+            {
+                HMNameLBL.Text = "Brx Ruiz";
+                HMMonikerLBL.Text = "The Seamanluluto of Bacolod";
+                HMStatusLBL.Text = "Evicted (3EN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_BrxRuiz.png");
+            }
+            else if (HMCB.Text == "Dingdong and Patrick")
+            {
+                HMNameLBL.Text = "Dingdong Bahan and Patrick Ramirez";
+                HMMonikerLBL.Text = "The Charismatic and Go-Getter Partners of Taguig and Maynila";
+                HMStatusLBL.Text = "Nominated (4NN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_Dongpat.png");
+            }
+            else if (HMCB.Text == "Dylan Yturralde")
+            {
+                HMNameLBL.Text = "Dylan Yturralde";
+                HMMonikerLBL.Text = "The Astig Volleybae of Pampanga";
+                HMStatusLBL.Text = "Nominated (4NN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_DylanYturralde.png");
+            }
+            else if (HMCB.Text == "Fyang Smith")
+            {
+                HMNameLBL.Text = "Fyang Smith";
+                HMMonikerLBL.Text = "The Anakabogerang Influencer of Mandaluyong";
+                HMStatusLBL.Text = "Nominated (4NN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_FyangSmith.png");
+            }
+            else if (HMCB.Text == "Jan Silva")
+            {
+                HMNameLBL.Text = "Jan Silva";
+                HMMonikerLBL.Text = "The Prodigal Bai of Cebu";
+                HMStatusLBL.Text = "Safe";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_JanSilva.png");
+            }
+            else if (HMCB.Text == "Jarren Garcia")
+            {
+                HMNameLBL.Text = "Jarren Garcia";
+                HMMonikerLBL.Text = "The Charming Crooner of London, United Kingdom";
+                HMStatusLBL.Text = "Safe";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_JarrenGarcia.png");
+            }
+            else if (HMCB.Text == "Jas Dudley-Scales")
+            {
+                HMNameLBL.Text = "Jas Dudley-Scales";
+                HMMonikerLBL.Text = "The Optimistic Ate of Dumaguete";
+                HMStatusLBL.Text = "Safe";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_JasDudleyScales.png");
+            }
+            else if (HMCB.Text == "JM Ibarra")
+            {
+                HMNameLBL.Text = "JM Ibarra";
+                HMMonikerLBL.Text = "The Pogisyanong Pilo-To Be of Quezon";
+                HMStatusLBL.Text = "Safe";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_JMIbarra.png");
+            }
+            else if (HMCB.Text == "Joli Alferez")
+            {
+                HMNameLBL.Text = "Joli Alferez";
+                HMMonikerLBL.Text = "The Smiling Darling of Camarines Sur";
+                HMStatusLBL.Text = "Safe";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_JoliAlferez.png");
+            }
+            else if (HMCB.Text == "JP Cabrera")
+            {
+                HMNameLBL.Text = "JP Cabrera";
+                HMMonikerLBL.Text = "The Perserving Bunso of Quezon City";
+                HMStatusLBL.Text = "Safe";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_JPCabrera.png");
+            }
+            else if (HMCB.Text == "Kai Montinola")
+            {
+                HMNameLBL.Text = "Kai Montinola";
+                HMMonikerLBL.Text = "The Singing Gwapa of Cebu";
+                HMStatusLBL.Text = "Nominated (4NN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_KaiMontinola.png");
+            }
+            else if (HMCB.Text == "Kanata Tapia")
+            {
+                HMNameLBL.Text = "Kanata Tapia";
+                HMMonikerLBL.Text = "The Lola-Ving Apo of Occidental Mindoro";
+                HMStatusLBL.Text = "Evicted (2EN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_KanataTapia.png");
+            }
+            else if (HMCB.Text == "Kolette Madelo")
+            {
+                HMNameLBL.Text = "Kolette Madelo";
+                HMMonikerLBL.Text = "The Ma-Cute-Lit na Raketera of General Santos";
+                HMStatusLBL.Text = "Nominated (4NN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_KoletteMadelo.png");
+            }
+            else if (HMCB.Text == "Marc Nanninga")
+            {
+                HMNameLBL.Text = "Marc Nanninga";
+                HMMonikerLBL.Text = "The Sporty Go Lucky Kuya of Camarines Norte";
+                HMStatusLBL.Text = "Evicted (2EN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_MarcNanninga.png");
+            }
+            else if (HMCB.Text == "Noimie Steikunas")
+            {
+                HMNameLBL.Text = "Noimie Steikunas";
+                HMMonikerLBL.Text = "The Mombitious Chikadora of Lithuania";
+                HMStatusLBL.Text = "Evicted (3EN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_NoimieSteikunas.png");
+            }
+            else if (HMCB.Text == "Rain Celmar")
+            {
+                HMNameLBL.Text = "Rain Celmar";
+                HMMonikerLBL.Text = "The Cheerfu-Langga of Cebu";
+                HMStatusLBL.Text = "Nominated (4NN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_RainCelmar.png");
+            }
+            else if (HMCB.Text == "Therese Villamor")
+            {
+                HMNameLBL.Text = "Therese Villamor";
+                HMMonikerLBL.Text = "The Determined Daughter of Camarines Sur";
+                HMStatusLBL.Text = "Evicted (1EN)";
+                HMPB.Image = new Bitmap("E:\\DRPSPHCALab\\PBBGen11HMInfo-CSharp\\PBB Gen 11 Housemates Info\\Resources\\HMPics\\PHCAPBBGen11HM_ThereseVillamor.png");
+            }
+        }
+    }
+}
